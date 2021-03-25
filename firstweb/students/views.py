577b63c86e7cc794827a5b7b9391270b45b746f4
devopsit51 <69233185+devopsit51@users.noreply.github.com>
2021-03-25 19:02:55 +0530

    update

---
+++ firstweb/students/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+def fee(request):
+    return render(request, "fees/fee.html")
+
+def register(request):
+    return render(request, "register.html")
+
+def login(request):
+    return HttpResponse("This is login page")
