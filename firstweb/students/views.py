2a03cbd6a614d661bcf2d4751f86c4dba88faa2f
devopsit51 <69233185+devopsit51@users.noreply.github.com>
2021-04-06 20:15:30 +0530

    upload

--- firstweb/students/views.py
+++ firstweb/students/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from students.models import Category
 # Create your views here.
 
 def index(request):
+
     return render(request, "index.html")
 
 def fee(request):
